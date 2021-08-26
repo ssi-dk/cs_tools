ae2c8a6ef954e4dcb331c719f8288c4911f9f39e
Finn Gruwier Larsen <finn@gruwier.dk>
2021-08-26 11:20:50 +0200

    Add class SampleContainer.

--- create_samplesheet.py
+++ create_samplesheet.py
@@ -14,6 +14,30 @@ Illumina standard:
 Author: Finn Gruwier Larsen, figl@ssi.dk
 """
 
+
+class SampleContainer:
+    _samples: dict = {}
+    _sample_sheet_path: pathlib.Path
+
+    def __init__(self):
+        self._sample_sheet_path = SAMPLE_SHEET_PATH
+        """Todo:
+            With SAMPLE_SHEET_PATH open for reading:
+            Add existing samples to _samples
+        """
+    
+    def add_sample(sample_name):
+        """Todo: add to _samples. Directory is defined in FASTQ_DIR. Filenames follow Illumina standard."""
+        pass
+
+    def save():
+        """Todo:
+            With SAMPLE_SHEET_PATH open for writing:
+            Write to file overwriting any existing version
+        """
+        pass
+
+
 def get_new_samples(fastq_dir: pathlib.Path):
     r1_files_gen = fastq_dir.glob("*_S*_R1_001.fastq*")
     while True:
@@ -28,13 +52,13 @@ parser.add_argument('-s', '--sample_sheet', help="Path and filename for global s
 parser.add_argument('-d', '--fastq_dir', help="Path to existing directory containing fastq files. Default: current directory.")
 args = parser.parse_args()
 try:
-    sample_sheet_path = pathlib.Path(args.sample_sheet or os.getenv('GLOBAL_SAMPLE_SHEET'))
+    SAMPLE_SHEET_PATH = pathlib.Path(args.sample_sheet or os.getenv('GLOBAL_SAMPLE_SHEET'))
 except TypeError:
     print("--sample_sheet not set and no value found for $GLOBAL_SAMPLE_SHEET. Exiting.")
     sys.exit(1)
-print(f"Sample sheet: {sample_sheet_path}")
-fastq_dir = pathlib.Path(args.fastq_dir or os.getcwd())
-print(f"Folder to add: {fastq_dir}")
+print(f"Sample sheet: {SAMPLE_SHEET_PATH}")
+FASTQ_DIR = pathlib.Path(args.fastq_dir or os.getcwd())
+print(f"Folder to add: {FASTQ_DIR}")
 
 """
 samples = SampleContainer(sample_sheet)
