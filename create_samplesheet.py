7b179fc9ba77f63614bb2f9972c287dc153476e8
Finn Gruwier Larsen <finn@gruwier.dk>
2021-08-26 09:43:19 +0200

    sample_sheet.

--- create_samplesheet.py
+++ create_samplesheet.py
@@ -1,6 +1,7 @@
 import argparse
 import pathlib
 import os
+import sys
 
 """
 Create and maintain a global sample sheet for chewieSnake.
@@ -14,10 +15,16 @@ Author: Finn Gruwier Larsen, figl@ssi.dk
 """
 
 parser = argparse.ArgumentParser(description="Create and maintain a global sample sheet for chewieSnake.")
-parser.add_argument('-d', '--fastq_dir', help="Path to existing directory containing fastq files. Default: current directory.")
 parser.add_argument('-s', '--sample_sheet', help="Path and filename for global sample sheet."
     "Default: value of envvar $GLOBAL_SAMPLE_SHEET. If file does not exist it will be created.")
+parser.add_argument('-d', '--fastq_dir', help="Path to existing directory containing fastq files. Default: current directory.")
 args = parser.parse_args()
+
+try:
+    sample_sheet = pathlib.Path(args.sample_sheet or os.getenv('GLOBAL_SAMPLE_SHEET'))
+except TypeError:
+    print("--sample_sheet not set and no value found for $GLOBAL_SAMPLE_SHEET. Exiting.")
+    sys.exit(1)
 
 fastq_dir = pathlib.Path(args.fastq_dir or os.getcwd())
 print(f"Looking for fastq files in {fastq_dir}.")
