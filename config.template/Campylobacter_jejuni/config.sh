80f95666cde9982a47ec071a2d8d793a58cc4a6b
Finn Gruwier Larsen <finn@gruwier.dk>
2021-09-03 15:13:45 +0200

    Unfix an error that was not an error.

--- config.template/Campylobacter_jejuni/config.sh
+++ config.template/Campylobacter_jejuni/config.sh
@@ -1,3 +1,3 @@
 export SPECIES=Campylobacter_jejuni
 export SCHEME=$PWD/schemes/campylobacter_cgmlst_chewbbacaonline
-export PRODIGAL=$CONDA_PREFIX/envs/chewiesnake/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/$SPECIES.trn
+export PRODIGAL=$CONDA_PREFIX/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/$SPECIES.trn
--- config.template/Salmonella_enterica/config.sh
+++ config.template/Salmonella_enterica/config.sh
@@ -1,3 +1,3 @@
 export SPECIES=Salmonella_enterica
 export SCHEME=$PWD/schemes/enterobase_senterica_cgmlst
-export PRODIGAL=$CONDA_PREFIX/envs/chewiesnake/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/$SPECIES.trn
+export PRODIGAL=$CONDA_PREFIX/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/$SPECIES.trn
