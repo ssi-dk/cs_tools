511ffa0d91f64fcd1fba7b6d550cd595ac091d9b
Finn Gruwier Larsen <finn@gruwier.dk>
2021-09-06 11:07:07 +0200

    Add parser.

--- datadumper.py
+++ datadumper.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pathlib
 
@@ -25,8 +26,8 @@ def update_distance_matrix(working_directory: pathlib.Path, species_name: str):
         elements_gen = line_splitter(line, ' ')
         sample_name = next(elements_gen)
         print("Sample name:", sample_name)
-        key = species_name + ':' + sample_name
-        print("Key:", key)
+        # key = species_name + ':' + sample_name
+        # print("Key:", key)
         # Make a Redis 'sorted set' entry with distances as scores and sample names as values
         # Todo: check for existing key.
         # r.zadd(key, {sample_name: next(elements_gen) for sample_name in sample_names})
@@ -34,3 +35,17 @@ def update_distance_matrix(working_directory: pathlib.Path, species_name: str):
 
         # Todo: add argument --working_directory (chewieSnake output directory)
         # Todo: add argument --species
+    return True
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Dump chewieSnake data to MongoDB.")
+    parser.add_argument('working_directory', help="Directory with output data from chewieSnake.")
+    parser.add_argument(
+        'species', help="Species name with underscore notation, case sensitive (e. g. 'Salmonella_enterica'). " + \
+        "A collection with this name will be created in MongoDB if it does not exist.")
+    args = parser.parse_args()
+    result = update_distance_matrix(args.working_directory, args.species)
+
+if __name__ == '__main__':
+    main()
