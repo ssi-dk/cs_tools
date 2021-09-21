581120d2314d653af1df0910e10ec0e963a88c99
Finn Gruwier Larsen <figl@ssi.dk>
2021-09-21 15:11:31 +0200

    Remove \ at end.

--- run_chewiesnake.sh
+++ run_chewiesnake.sh
@@ -5,4 +5,4 @@ chewiesnake -t 10 --reads \
 --sample_list /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/sample_list.tsv \
 --scheme /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/schemes/enterobase_senterica_cgmlst \
 --prodigal $CONDA_PREFIX/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/Salmonella_enterica.trn \
---working_directory /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/output \
+--working_directory /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/output
