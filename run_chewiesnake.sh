bf2ed6058a75a7af147bf1282a6dfcd64995dbb0
Finn Gruwier Larsen <figl@ssi.dk>
2021-09-23 10:53:56 +0200

    Only load modules if not in dev env.

--- run_chewiesnake.sh
+++ run_chewiesnake.sh
@@ -9,8 +9,10 @@
 ### Specify resources
 #PBS -l nodes=1:ppn=4,mem=12gb,walltime=12:00:00,advres=fvst_ssi_dtu_wiki_fodevarestyrelsen.16
 
-module load tools
-module load chewiesnake/3.0.0
+if ! [[-z $DEV_ENV ]]; then
+  module load tools
+  module load chewiesnake/3.0.0
+fi
 
 cmd="chewiesnake -t 10 --reads \
 --sample_list /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/sample_list.tsv \
@@ -18,13 +20,11 @@ cmd="chewiesnake -t 10 --reads \
 --prodigal $CONDA_PREFIX/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/Salmonella_enterica.trn \
 --working_directory /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/output"
 
-if [ "$1" == "--dryrun" ]
-then
+if [ "$1" == "--dryrun" ]; then
  cmd="${cmd} --dryrun"
 fi
 
-if [ "$1" == "--unlock" ]
-then
+if [ "$1" == "--unlock" ]; then
  cmd="${cmd} --unlock"
 fi
 
