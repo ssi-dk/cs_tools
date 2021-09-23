a4ce425d44341a6113e7db0f16ec0f595c8c665b
Finn Gruwier Larsen <figl@ssi.dk>
2021-09-23 10:15:01 +0200

    Add comments.

--- run_chewiesnake.sh
+++ run_chewiesnake.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+### Specify group name to resource manager
 #PBS -A fvst_ssi_dtu
-#PBS -l nodes=1:ppn=4,mem=12gb,walltime=12:00:00,advres=fvst_ssi_dtu_wiki_fodevarestyrelsen.16
+
+### Specify group name to resource manager
 #PBS -W x=advres:fvst_ssi_dtu_wiki_fodevarestyrelsen.16
+
+### Specify resources
+#PBS -l nodes=1:ppn=4,mem=12gb,walltime=12:00:00,advres=fvst_ssi_dtu_wiki_fodevarestyrelsen.16
+
 module load tools
 module load chewiesnake/3.0.0
 chewiesnake -t 10 --reads \
