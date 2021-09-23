9592e3a527d5a5bff568657e7836c91cc7089578
Finn Gruwier Larsen <figl@ssi.dk>
2021-09-23 10:37:16 +0200

    Add dryrun option.

--- run_chewiesnake.sh
+++ run_chewiesnake.sh
@@ -11,8 +11,16 @@
 
 module load tools
 module load chewiesnake/3.0.0
-chewiesnake -t 10 --reads \
+
+cmd="chewiesnake -t 10 --reads \
 --sample_list /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/sample_list.tsv \
 --scheme /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/schemes/enterobase_senterica_cgmlst \
 --prodigal $CONDA_PREFIX/opt/chewiesnake/chewBBACA/CHEWBBACA/prodigal_training_files/Salmonella_enterica.trn \
---working_directory /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/output
+--working_directory /home/projects/fvst_ssi_dtu/test_data/cs/Salmonella_enterica/output"
+
+if [ "$1" == "--dryrun" ]
+then
+ cmd="${cmd} --dryrun"
+fi
+
+$cmd
